3aef1aa260eac08559dd6d3a3151d0c7fc446c93
minor-e <estebanatmail@gmail.com>
2017-04-04 20:29:41 +0200

    First exercise completed.

---
+++ ex1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+"""Hi,  this is the first exercise of many from the Learn Python the Hard Way Third Edition book"""
+
+# Ex.1 keyword print
+print "Hello World!"
+# Double quotations signs rules
+print "This line accepts apostrophes like: it is --> it's."
+# Single quotations signs escapes may character
+print 'This line here cannot print apostrophes without escape characters like backslash it is --> it\'s'
+# Check-out this letters!
+print "The swedish alphabet contain three extra letters; ",u"деж"
+# Fancy prompt
+print 100 * "*", "I'm a star", 100*"*"
+# Simple string concat
+print "Ghost/" + "\in/" + "\Shell"
+# Checking out spaces
+print "Tab --> thisshouldbe --> this\tshould\tbe"
+print "This is wrong: T A B --> TAB --> T \bA \bB"
+print "This is also wrong: TCAB --> better start from the beginning and correct the line line\rTAB is great!"
+print "This space here--> a\fb is different from this --> a b"
+print"I'm need an new line here --> No! Line should break --> No!\nLine should break"
+print 5*"A","\n", 4*"\vB","\n",3*"\v\vC"
+print r"I'm raw string!"
+print "\aAlert bell"
+
+
+
+
+
+
